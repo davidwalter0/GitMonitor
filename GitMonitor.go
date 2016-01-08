f3533c0932e7be54b745f6549f5d19246a559c47
Leandro Taveras <ltaveras@clypd.com>
2016-01-08 16:11:03 -0500

    fixed + understood permissions issues

--- GitMonitor.go
+++ GitMonitor.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"time"
 
@@ -13,14 +12,15 @@ import (
 TODO
 Testing?
 Find a way to get commit message? -> DONE
-	Need to figure out why it crashes/panics after any more than 3 iterations
+	Need to figure out why it crashes/panics after any more than 3 iterations -> DONE
+	Note to self: Can access commit data and IFF it's for your account and repo, else not allowed + crashes
 Find a way to read the list of events so they print in chronological order -> DONE
 Abstract --> DONE
 
 
 */
 
-const minsToWait int = 5 // Number of minutes to wait between each update
+const minsToWait int = 1 // Number of minutes to wait between each update
 
 // A structure to keep track of relevant data
 type GitHubEvent struct {
@@ -35,30 +35,34 @@ type GitHubEvent struct {
 
 // Overload the String() function to present the data in an easy-to-parse manner
 func (g GitHubEvent) String() string {
-	return fmt.Sprintf("- %v (%v) commmited a %v on the repo %q with message %q and ID %q (public: %v)", g.Name, g.Username, g.Type, g.Repo, g.Message, g.EventID, g.Public)
+	return fmt.Sprintf("- %v commmited a %v on the repo %q and ID %q (public: %v)", g.Username, g.Type, g.Repo, g.EventID, g.Public)
 }
 
 // Returns a GitHubEvent struct from the go-github github.Event struct
 func getGitHubEvent(cur github.Event) GitHubEvent {
-	var data map[string]interface{}                                // Declare a map to parse the json
+	/*v Note: For below data, see note in todo section
+	ar data map[string]interface{}                                // Declare a map to parse the json
 	if err := json.Unmarshal(*cur.RawPayload, &data); err != nil { // Unmarshal the JSON
 		panic(err)
 	}
 	commitArray := data["commits"].([]interface{})       // Cast the "commit" entry of the map to an array
 	commitMap := commitArray[0].(map[string]interface{}) // Grab the first element of the array, cast it to a map[string]interface{} representing the commit information
+	fmt.Println(commitMap)
 	/*
 		Other fields in commitmap besides "message" include: "distinct", url", "sha", "author" (returns a map with fields "email" and "name") )
 	*/
-	commitMessage := commitMap["message"].(string) // Typecast interface{} to string
-	authorMap := commitMap["author"].(map[string]interface{})
-	authorName := authorMap["name"].(string) // Typecast interface{} to string
-	return GitHubEvent{authorName, *cur.Actor.Login, *cur.Type, *cur.Repo.Name, *cur.ID, *cur.Public, commitMessage}
+	//commitMessage := commitMap["message"].(string) // Typecast interface{} to string
+	//fmt.Println(commitMessage)
+	//authorMap := commitMap["author"].(map[string]interface{})
+	//fmt.Println(authorMap)
+	//authorName := authorMap["name"].(string) // Typecast interface{} to string
+	return GitHubEvent{"authorName", *cur.Actor.Login, *cur.Type, *cur.Repo.Name, *cur.ID, *cur.Public, "commitMessage"}
 }
 
 // Listen for events on a particular repo, updates every minsToWait mins
 func ListenForEvents(owner, repo string, ch chan<- GitHubEvent) {
 	ts := oauth2.StaticTokenSource( // Set up client
-		&oauth2.Token{AccessToken: "2f39da9a4134b244b909fa38527005a2c953b9c7"}) // Insert your personal access token here
+		&oauth2.Token{AccessToken: ""}) // Insert your personal access token here
 	tc := oauth2.NewClient(oauth2.NoContext, ts)
 	client := github.NewClient(tc)
 	var prev GitHubEvent
@@ -67,22 +71,24 @@ func ListenForEvents(owner, repo string, ch chan<- GitHubEvent) {
 		if repo != "" {                                                              // If repo is not nil, monitory repo events instead
 			events, _, _ = client.Activity.ListRepositoryEvents(owner, repo, nil)
 		}
-		cur := getGitHubEvent(events[0])
-		if cur != prev {
-			mostRec := cur
+
+		first := getGitHubEvent(events[0])
+		if first != prev {
+			mostRec := first
 			toSendToChan := make([]GitHubEvent, 0) // Add all events to a slice
-			for i := 0; i < 3; i++ {
-				rn := getGitHubEvent(events[i])
-				if rn == prev {
+			for _, val := range events {
+				cur := getGitHubEvent(val)
+				if cur == prev {
 					break
 				}
-				toSendToChan = append(toSendToChan, rn)
+				toSendToChan = append(toSendToChan, cur)
 			}
 			for i, _ := range toSendToChan {
 				ch <- toSendToChan[len(toSendToChan)-i-1] // Sends items to channel in reverse order so new events appear chronologically at the bottom
 			}
 			prev = mostRec // Update prev to the most recently encountered item on the list
 		}
+
 		time.Sleep(time.Minute * time.Duration(minsToWait)) // Sleep for minsToWait mins
 	}
 }
